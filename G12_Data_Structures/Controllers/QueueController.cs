f41f74e8bfdcfd8b5cb59e106f1807e5538b759a
Radley Nelson <ern23@byu.edu>
2018-10-15 23:03:39 -0600

    Added Comments and Fixed Add one action to Dictionary

--- G12_Data_Structures/Controllers/QueueController.cs
+++ G12_Data_Structures/Controllers/QueueController.cs
@@ -8,33 +8,42 @@ namespace DataStructures.Controllers
 {
     public class QueueController : Controller
     {
+        //Initalize Que Object and other Variables
         public static Queue<int> myQueue = new Queue<int>();
         public static string SearchResults;
         public static int queueValue;
 
         public static double dTime = 0;
         public static bool DisplayModel = false;
+
+        //Index Method that directs to Index View.
         public ActionResult Index()
         {
-            if(SearchResults != null)
+            //If we have added a searh results thorugh the Search Method we want to add them to the View Bag
+            if (SearchResults != null)
             {
                 ViewBag.Search = SearchResults;
             }
+            //If  the Display Method is called then we want to add something to the ViewBag to show the modal
             if (DisplayModel)
             {
                 ViewBag.DisplayModel = true;
             }
+            //Pass in the time it took to Search
             ViewBag.StopWatch = dTime;
             DisplayModel = false;
             SearchResults = null;
+            //Added Queue to the ViewBat
             ViewBag.Queue = myQueue;
             return View();
         }
+        //Mehtod to Add one to queue
         public ActionResult AddOne()
         {
             myQueue.Enqueue(myQueue.Count + 1);
             return RedirectToAction("Index");
         }
+        //Add 2000 to Queue
         public ActionResult AddHugeList()
         {
             myQueue.Clear();
@@ -45,11 +54,14 @@ namespace DataStructures.Controllers
             }
             return RedirectToAction("Index");
         }
+
+        //Clear out the Queue
         public ActionResult Clear()
         {
             myQueue.Clear();
             return RedirectToAction("Index");
         }
+        //Delete form off the Top of the Queue
         public ActionResult DeleteFrom()
         {
             if (myQueue.Count > 0)
@@ -59,10 +71,11 @@ namespace DataStructures.Controllers
 
             return RedirectToAction("Index");
         }
+        //Search for the value 3 in the Queue
         public ActionResult Search()
         {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-
+            //Start Stop Watch
             sw.Start();
 
             //loop to do all the work
@@ -75,7 +88,7 @@ namespace DataStructures.Controllers
             {
                 SearchResults = "f";
             }
-
+            //Stop Stopwatch
             sw.Stop();
 
             TimeSpan ts = sw.Elapsed;
@@ -87,6 +100,7 @@ namespace DataStructures.Controllers
             
 
         }
+        //Display the Queue in the Modal
         public ActionResult Display()
         {
             DisplayModel = true;
